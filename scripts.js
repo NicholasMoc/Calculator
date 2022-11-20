308930397a726c209e40000690f8433330b08a76
NicholasMoc <102491443+NicholasMoc@users.noreply.github.com>
2022-11-20 01:50:48 -0800

    Added ablity to apply a single operation. Need to implement string of operations

--- scripts.js
+++ scripts.js
@@ -15,5 +15,113 @@ divide = (a,b) => {
 }
 
 function operate(op, a, b) {
-   return op(a, b);
+    console.log(a);
+    console.log(b);
+    a = parseFloat(a);
+    b = parseFloat(b);
+    return op(a, b);
 }
+
+let display = document.querySelector('.screen');
+let total = 0;
+let operatorCount = 0;
+let one = '';
+let two = '';
+let currentOp = '';
+let first = true;
+
+function initializeNumbers() {
+    let buttons = document.querySelectorAll('#number');
+    buttons.forEach((but) => {
+        but.addEventListener('click', () => {
+            if(first)
+            {
+                one += but.innerHTML;
+                display.textContent = one;
+            }
+            else if (!two) {
+                display.textContent = ''
+                two += but.innerHTML
+                display.textContent = two;
+            }
+            else {
+                two += but.innerHTML
+                display.textContent = two;
+            }
+        })
+    })
+}
+
+function initializeOperators() {
+    let operators = document.querySelectorAll('.operator');
+    operators.forEach((op) => {
+        op.addEventListener('click', () => {
+            if(!currentOp)
+            {
+                first = false;
+                currentOp = op.id;
+                console.log(currentOp);
+            }
+            if(!two) {
+                
+            }
+            if(operatorCount === 0 && two)
+            {
+                if(currentOp === 'add')
+                {
+                    total = operate(add, one, two);
+                }
+                else if(currentOp === 'subtract')
+                {
+                    total = operate(subtract, one, two);
+                }
+                else if(currentOp === 'multiply')
+                {
+                    total = operate(multiply, one, two);
+                }
+                else if(currentOp === 'divide')
+                {
+                    total = operate(divide, one, two);
+                }
+                display.textContent = total;
+                two = '';
+                operatorCount += 1;
+                currentOp = op.id;
+            }
+            if(operatorCount > 1)
+            {
+                if(currentOp === 'add')
+                {
+                    total = operate(add, total, two);
+                }
+                else if(currentOp === 'subtract')
+                {
+                    total = operate(subtract, total, two);
+                }
+                else if(currentOp === 'multiply')
+                {
+                    total = operate(multiply, total, two);
+                }
+                else if(currentOp === 'divide')
+                {
+                    total = operate(divide, total, two);
+                }
+                two = '';
+                display.textContent = total;
+                currentOp = op.id;
+            }
+        })
+    })
+}
+
+function initialize()
+{
+    initializeNumbers();
+    initializeOperators();
+}
+
+initialize();
+
+
+
+
